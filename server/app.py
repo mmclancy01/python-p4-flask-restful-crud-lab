cea28218a509bc4f02c962fa6ce62911ca070fc7
Matthew Clancy <mmclancy01@yahoo.com>
2024-04-08 12:33:13 -0400

    fin lab

--- server/app.py
+++ server/app.py
@@ -46,7 +46,22 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        
+        for d in data:
+            setattr(plant, d, data[d])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
